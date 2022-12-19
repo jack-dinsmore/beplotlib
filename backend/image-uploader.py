9beeecc5d65140533fd61f88aaa186c823e0ba41
Kaveh Pezeshki <kaveh@DN0a24a851.SUNet>
2022-12-19 14:54:09 -0800

    fixed chatgpt weirdness, added notes about 'secure' ID implementation

--- backend/image-uploader.py
+++ backend/image-uploader.py
@@ -1,12 +1,21 @@
 from flask import Flask, request
 import os
 
+# fix up chatgpt weirdness here and use dynamic route targets
+
+# each potential upload is assigned an ID, which is stored in some large file
+
+# dynamically generate upload target in form uploader with some ID. if this ID has not yet been used, allow the upload. Otherwise, do not allow the upload.
+
 app = Flask(__name__)
 
 UPLOAD_FOLDER = '/path/to/upload/folder'
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
+@app.route('/upload/<int:upload_id>', methods=['POST'])
+def upload_file(Number):
+
+  # check here if upload_id is a permissible ID
+
   if request.method == 'POST':
     # check if the post request has the file part
     if 'image' not in request.files:
@@ -17,6 +26,10 @@ def upload_file():
     if file.filename == '':
       return 'No selected file'
     if file:
+
+
+      # here, run through imagemagick (all files), rescale to smaller size (if >100kB)
+
       filename = file.filename
       file.save(os.path.join(UPLOAD_FOLDER, filename))
       return 'File uploaded successfully'
